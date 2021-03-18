ac2f9d901084420740c61f874418bd8e88eeb3a1
Steffen Kretschmann <steffen.kretschmann@fmgarage.com>
2021-03-18 11:32:40 +0100

    update: omit NetworkManager, config file,

--- build/install_fms.sh
+++ build/install_fms.sh
@@ -1,31 +1,29 @@
 #!/usr/bin/env bash
 
-
+# settings
 build_dir_mount=/root/build/
-# rpm_name=filemaker_server-19.1.2-234.x86_64.rpm
 # rpm_name=filemaker_server-19.2.1-23.x86_64.rpm
 # rpm_name=fms-linux-912.rpm
 rpm_name=fms-linux-921.rpm
-url=$(cat ${build_dir_mount}url.txt)
+url=$(grep "url=" ${build_dir_mount}config.txt | sed 's/.*=//')
+cert_prefix=$(grep "cert_prefix=" ${build_dir_mount}config.txt | sed 's/.*=//')
 package_url=$url$rpm_name
 package_remove=false
-assisted_install=assisted_install.txt
-fms_admin_user=$(grep "Admin Console User=" ${build_dir_mount}assisted_install.txt | sed 's/.*=//')
-fms_admin_pass=$(grep "Admin Console Password=" ${build_dir_mount}assisted_install.txt | sed 's/.*=//')
-cert_prefix=$(cat ${build_dir_mount}cert_prefix.txt)
+assisted_install=$(grep "assisted_install=" ${build_dir_mount}config.txt | sed 's/.*=//')
+fms_admin_user=$(grep "Admin Console User=" ${build_dir_mount}${assisted_install} | sed 's/.*=//')
+fms_admin_pass=$(grep "Admin Console Password=" ${build_dir_mount}${assisted_install} | sed 's/.*=//')
 
 # color prompt global
 echo "PS1='\[\033[02;32m\]\u@\H:\[\033[02;34m\]\w\$\[\033[00m\] '" >> /etc/bashrc
 
-# install CentOS SCLo RH repository for httpd24
+# install CentOS SCLo repository for httpd24
 yum install centos-release-scl -y
-# yum install centos-release-scl-rh -y
 
 # update
 yum update -y
 
 # pre packages, possibly omit sudo, autofs
-yum install bash-completion NetworkManager firewalld nano policycoreutils net-tools httpd24 httpd24-mod_ssl -y
+yum install bash-completion firewalld nano policycoreutils net-tools httpd24 httpd24-mod_ssl -y
 
 # download filemaker_server package
 if [[ ! -f "${build_dir_mount}""${rpm_name}" ]]; then
@@ -52,7 +50,7 @@ fmsadmin certificate import -yu "${fms_admin_user}" -p "${fms_admin_pass}" --key
 
 # remove install packages
 printf "\nremove install packages\n"
-rm -rv /root/deps/
+rm -r /root/deps/
 if [[ $package_remove ]]; then
     rm -r ${build_dir_mount}${rpm_name}
 fi

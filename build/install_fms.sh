33bcf88889bbd509e93183f37df299063f8393a7
Steffen Kretschmann <steffen.kretschmann@fmgarage.com>
2021-03-26 17:18:10 +0100

    update: config file, package checking, error handling

--- build/install_fms.sh
+++ build/install_fms.sh
@@ -2,58 +2,59 @@
 
 # settings
 build_dir_mount=/root/build/
-# rpm_name=filemaker_server-19.2.1-23.x86_64.rpm
-# rpm_name=fms-linux-912.rpm
-rpm_name=fms-linux-921.rpm
-url=$(grep "url=" ${build_dir_mount}config.txt | sed 's/.*=//')
+# url=$(grep "url=" ${build_dir_mount}config.txt | sed 's/.*=//')
 cert_prefix=$(grep "cert_prefix=" ${build_dir_mount}config.txt | sed 's/.*=//')
-package_url=$url$rpm_name
-package_remove=false
+# package_url=$url
+package_remove=$(find "$build_dir_mount" -name package_remove)
 assisted_install=$(grep "assisted_install=" ${build_dir_mount}config.txt | sed 's/.*=//')
-fms_admin_user=$(grep "Admin Console User=" ${build_dir_mount}${assisted_install} | sed 's/.*=//')
-fms_admin_pass=$(grep "Admin Console Password=" ${build_dir_mount}${assisted_install} | sed 's/.*=//')
+fms_admin_user=$(grep "Admin Console User=" ${build_dir_mount}"${assisted_install}" | sed 's/.*=//')
+fms_admin_pass=$(grep "Admin Console Password=" ${build_dir_mount}"${assisted_install}" | sed 's/.*=//')
 
 # color prompt global
 echo "PS1='\[\033[02;32m\]\u@\H:\[\033[02;34m\]\w\$\[\033[00m\] '" >> /etc/bashrc
 
 # install CentOS SCLo repository for httpd24
-yum install centos-release-scl -y
+yum install centos-release-scl -y || exit 1
 
 # update
 yum update -y
 
 # pre packages, possibly omit sudo, autofs
-yum install bash-completion firewalld nano policycoreutils net-tools httpd24 httpd24-mod_ssl -y
+yum install bash-completion firewalld nano policycoreutils net-tools httpd24 httpd24-mod_ssl -y || exit 1
 
 # download filemaker_server package
-if [[ ! -f "${build_dir_mount}""${rpm_name}" ]]; then
-    printf "\ndownloading fms package ...\n"
-    package_remove=true
-    curl "${package_url}" --output ${build_dir_mount}${rpm_name}
+package=$(find "$build_dir_mount" -name "*.rpm")
+if [[ ! $package ]]; then
+    printf "no rpm package found"
+    exit 1
 fi
 
 # preload dependencies
-yum install --downloadonly --downloaddir=/root/deps ${build_dir_mount}${rpm_name} -y
+yum install --downloadonly --downloaddir=/root/deps "${package}" -y || exit 1
 
 # preinstall dependencies
 yum install root/deps/* -y
 
 # install filemaker_server
-FM_ASSISTED_INSTALL="${build_dir_mount}""${assisted_install}" yum install "${build_dir_mount}""${rpm_name}" -y
+FM_ASSISTED_INSTALL="${build_dir_mount}""${assisted_install}" yum install "${package}" -y || exit 1
 
 # check
 systemctl
 
 # import cert
-printf "\nimport certificate\n"
-fmsadmin certificate import -yu "${fms_admin_user}" -p "${fms_admin_pass}" --keyfile ${build_dir_mount}"${cert_prefix}".key --intermediateCA ${build_dir_mount}"${cert_prefix}".ca-bundle ${build_dir_mount}"${cert_prefix}".crt
+if [[ $cert_prefix ]]; then
+    printf "\nimport certificate\n"
+    fmsadmin certificate import -yu "${fms_admin_user}" -p "${fms_admin_pass}" --keyfile ${build_dir_mount}"${cert_prefix}".key --intermediateCA ${build_dir_mount}"${cert_prefix}".ca-bundle ${build_dir_mount}"${cert_prefix}".crt
+fi
 
 # remove install packages
 printf "\nremove install packages\n"
 rm -r /root/deps/
 if [[ $package_remove ]]; then
-    rm -r ${build_dir_mount}${rpm_name}
+    printf "\nremove fms package\n"
+    rm -r "${package}" "$package_remove"
 fi
 
-# OR exit
-exit
+echo "" > ${build_dir_mount}build_success
+# exit
+exit 0
--- build/make_image.sh
+++ build/make_image.sh
@@ -2,23 +2,63 @@
 
 
 # go to working dir
-cd "${0%/*}" || exit
+pwd="$( cd "$( dirname "${BASH_SOURCE[0]}" )"  && pwd )" || exit
+cd "$pwd" || exit
 
-pwd=$(pwd)
+# image name according
+cert_prefix=$(grep "cert_prefix=" ./config.txt | sed 's/.*=//')
+if [[ ! $cert_prefix ]]; then
+    image_name=centos-fms-19_2
+else
+    image_name=centos-fms-c-19_2
+fi
+
 build_image_name=fmsinstall
-image_name=centos-fms-19_2
+# todo pin version tag or is 7 sufficient?
+base_image=jrei/systemd-centos:7
 date=$(date +%Y-%m-%d)
 
+# check install package
+# download filemaker_server package
+package=$(find . -name "*.rpm")
+plines=$(wc -l <<< "$package" )
+if [[ ! $package ]]; then
+    printf "\ndownloading fms package ...\n"
+    url=$(grep "url=" ./config.txt | sed 's/.*=//')
+        STATUS=$(curl -s --head --output /dev/null -w '%{http_code}' "$url")
+        if [ ! $STATUS -eq 200 ]; then
+            echo "Got a $STATUS from $url ..."
+            exit
+        fi
+    curl "${url}" -O || exit
+    echo "" > ./package_remove
+    # package=$(find . -name "*.rpm")
+elif [[ $plines -gt 1 ]]; then
+    printf "%s rpm packages found, 1 expected" "$plines"
+    exit 1
+fi
+
+
 # check if container names are in use
-docker ps -aq --filter "name=${build_image_name}" | grep -q . && echo another build container already exists && exit 1 || printf "\r"
-docker ps -aq --filter "name=fmserver" | grep -q . && echo another fmserver container already exists && docker stop fmserver && docker rm fmserver || printf "\r"
-# docker ps -aq --filter "name=fmserver" | grep -q . && echo another fmserver container already exists && exit 1 || printf "\r"
+docker ps -aq --filter "name=${build_image_name}" | grep -q . && echo another build container already exists, removing... && docker stop $build_image_name && docker rm $build_image_name || printf "\r"
+docker ps -aq --filter "name=fmserver" | grep -q . && echo another fmserver container already exists, removing... && docker stop fmserver && docker rm fmserver || printf "\r"
+
 
 # build container
-docker run -d --name $build_image_name --cap-add=SYS_ADMIN --tmpfs /tmp --tmpfs /run --tmpfs /run/lock -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v "${pwd}":/root/build/ jrei/systemd-centos:7
+docker run -d --name $build_image_name --cap-add=SYS_ADMIN --tmpfs /tmp --tmpfs /run --tmpfs /run/lock -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v "${pwd}":/root/build/ "$base_image"
 
 # run install script in build container
 docker exec -ti $build_image_name /root/build/install_fms.sh
+
+# check for flag file
+build_success=$(find . -name build_success)
+if [[ ! $build_success ]]; then
+    echo "build not successful"
+    exit 1
+fi
+
+# remove flag file
+rm "$build_success" || exit 1
 
 # docker commit
 printf "\ncommit build container to new image ...\n"
@@ -29,13 +69,4 @@ docker tag $image_name:"${date}" "${image_name}":latest
 printf "\nremoving build container ...\n"
 docker stop $build_image_name && docker rm $build_image_name
 
-# start fmserver
-# TODO mount Databases
-# printf "\nstart filemaker server container ...\n"
-# docker run -d --name fmserver --cap-add=SYS_ADMIN --tmpfs /tmp --tmpfs /run --tmpfs /run/lock -v /sys/fs/cgroup:/sys/fs/cgroup:ro -p 5003:5003 -p 16000-16002:16000-16002 -p 80:80 -p 443:443 $image_name:latest || exit 1
-
-# check
-# printf "\ncheck port 16000 ...\n"
-# curl 127.0.0.1:16000
-
-printf "\ndone\n"
+printf "\nDone. You can now start your server with \e[34mdocker-compose up [-d] fms\e[39m or \e[34mfms-c\e[39m if you installed certificates.\n"
